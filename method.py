08f779925c29979630f02281149e7a649415cd2e
unnikuttan <unnikuttancr739@gmail.com>
2026-05-18 14:18:46 +0530

    method

---
+++ method.py
@@ -0,0 +1,18 @@
+# class person:
+#     def __init__(self, name):
+#        self.name = name
+       
+#     def greet(self):
+#       print("Hello,my name is " + self.name)
+
+# p1 = person("emil")
+# p1.greet()
+class person:
+    def __init__(self, name):
+       self.name = name
+       
+    def greet(self):
+      print("Hello,my name is " + self.name)
+
+p2 = person("Deep")
+p2.greet()
